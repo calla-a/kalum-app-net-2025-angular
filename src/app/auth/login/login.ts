1c1f7b1d638021dab1e897268e13fa15e383f4d4
calla-a <rrosalesguzmn@byupathway.edu>
2025-10-11 11:20:26 -0600

    Integracion de login con Api AuthKalum

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -7,6 +7,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { AuthService } from '../auth';
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-login',
@@ -21,10 +23,11 @@ import Swal from 'sweetalert2';
   templateUrl: './login.html',
   styles: ``
 })
-export class Login implements OnInit{
+export class Login implements OnInit {
   form!: FormGroup;
+  user: User = new User();
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(private fb: FormBuilder, private router: Router, private authService: AuthService) {
 
   }
 
@@ -36,16 +39,41 @@ export class Login implements OnInit{
   }
 
   onSubmit() {
-    if(this.form.valid) {
-      Swal.fire({
-        title: 'Login',
-        icon: 'success',
-        draggable: true
-      }).then((result) => {
-        if(result.isConfirmed) {
-          this.router.navigate(['/'])
-        }
-      });
+    if (this.form.valid) {
+      if (this.form.valid) {
+        this.user.username = this.form.get('username')?.value;
+        this.user.password = this.form.get('password')?.value;
+        this.authService.login(this.user).subscribe({
+          next: (response: any) => {
+            if(response.success) {
+              this.authService.saveToken(response.data.token);
+              const payload = this.authService.getPayload(response.data.token);
+              console.log(payload);
+              this.authService.saveUser(payload);
+
+              Swal.fire({
+                title: 'Login',
+                text: `¡Bienvenido al sistema ${this.user.username}!`,
+                icon: 'success',
+              }).then(result => {
+                if(result.isConfirmed) {
+                  this.router.navigate(['/'])
+                };
+              });
+            }
+          }, error: (data: any) => {
+            Swal.fire({
+              title: 'Login failed',
+              text: data.error.errors,
+              icon: 'question',
+            }).then((result) => {
+              if (result.isConfirmed) {
+                this.router.navigate(['/login'])
+              }
+            });
+          }
+        });
+      }
     }
   }
 }
--- src/app/layout/menu/menu.ts
+++ src/app/layout/menu/menu.ts
@@ -4,6 +4,8 @@ import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule} from '@angular/material/icon';
 import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../auth/auth';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-menu',
@@ -21,7 +23,7 @@ export class Menu {
 
   @Output() toggleSidenav = new EventEmitter<void>();
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private authService: AuthService) {
 
   }
 
@@ -29,13 +31,19 @@ export class Menu {
     this.toggleSidenav.emit();
   }
 
-  isLogedIn() {
-    return this.loggin;
-  }
-
   loginLogout() {
-    if(this.isLogedIn()) {
-      this.router.navigate(['/login'])
+    if(this.authService.isAuthenticated()) {
+      let username = this.authService.user.username;
+      Swal.fire({
+        title: 'Logout',
+        text: `${username}, has cerrado sesión con éxito`,
+        icon: 'success'
+      }).then(result => {
+        if(result.isConfirmed) {
+          this.authService.logout();
+          this.router.navigate(['/login']);
+        }
+      });
     }
   }
 }
