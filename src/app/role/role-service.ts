265cf228b0db12f8caa0cf5996bd541fb41ba946
calla-a <rrosalesguzmn@byupathway.edu>
2025-10-25 02:09:34 -0600

    Agregando service role

---
+++ src/app/role/role-service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environmnet } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RoleService {
+  endPointRole = environmnet.BASE_URL_KALUM_AUTH;
+
+  constructor(private httpclient: HttpClient) {
+    
+  }
+  
+  getListRole() : Observable<any> {
+    return this.httpclient.get<any[]>(`${this.endPointRole}/roles`)
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environmnet = {
+    production: false,
+    BASE_URL_KALUM_AUTH: 'http://api.kalu.adu.gt/kalum-auth/v1',
+    URL_ROLE: 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role',
+}
